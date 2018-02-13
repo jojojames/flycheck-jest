54eb3e3267190fd00b829e37ca1aba410003e23e
James Nguyen <james@jojojames.com>
2018-02-12 22:23:04 -0800

    Tweak test helper

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,22 @@
 ;;; test-helper.el --- Helpers for flycheck-jest-test.el
 
+;; Copyright (C) 2017 James Nguyen
+
+;; Author: James Nguyen <james@jojojames.com>
+;; Maintainer: James Nguyen <james@jojojames.com>
+;; Created: 20 November 2017
+
+;;; Commentary:
+
+;; Utilities for running flycheck-jest tests.
+
+;;; Code:
+(require 'ert)
+
+;; FIXME: Adding `f' as a dependency just for this line.
+(require 'f)
+(let ((flycheck-jest-dir (f-parent (f-dirname (f-this-file)))))
+  (add-to-list 'load-path flycheck-jest-dir))
+(require 'flycheck-jest)
+
 ;;; test-helper.el ends here
